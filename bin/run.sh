e7bb010becf7cf34b37dd6b19bbfaef68151b91d
MWCastle <matt.castle.w@gmail.com>
2023-02-17 11:52:33 -0500

    Dockerfile/Gemfile work to assist analyze-unknowns dev

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sleep 5s
 #gpg2 --keyserver hkp://keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 #\curl -sSL https://get.rvm.io -o rvm.sh
 #cat rvm.sh | bash -s stable --rails
